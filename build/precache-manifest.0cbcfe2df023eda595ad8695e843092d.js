1ce3473479f7b2d28a5e76413ea3c8216a3995ba
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-05-13 00:20:25 -0500

    update /build folder

---
+++ build/precache-manifest.0cbcfe2df023eda595ad8695e843092d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a5e64ca59958440120e3bb1d8e79f8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "afa1968fe5eae9ddf678",
+    "url": "/react-portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "37a3e4188c7729ebd432",
+    "url": "/react-portfolio/static/css/main.f525b05b.chunk.css"
+  },
+  {
+    "revision": "afa1968fe5eae9ddf678",
+    "url": "/react-portfolio/static/js/2.74592b84.chunk.js"
+  },
+  {
+    "revision": "ff0cf270dd8616b21f53257590f86887",
+    "url": "/react-portfolio/static/js/2.74592b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a3e4188c7729ebd432",
+    "url": "/react-portfolio/static/js/main.37b6aacb.chunk.js"
+  },
+  {
+    "revision": "4f2fc26c165b9cc33ce0",
+    "url": "/react-portfolio/static/js/runtime-main.63d2ede2.js"
+  },
+  {
+    "revision": "e5e47a44b486b17b6f5836aca2cb4027",
+    "url": "/react-portfolio/static/media/js-logo.e5e47a44.png"
+  },
+  {
+    "revision": "b32ab39f0e0ec0ee211e7d5871a85b25",
+    "url": "/react-portfolio/static/media/python-logo.b32ab39f.png"
+  },
+  {
+    "revision": "bc7784a824b0c7869eb878a6505e38f8",
+    "url": "/react-portfolio/static/media/wp-logo.bc7784a8.png"
+  }
+]);
